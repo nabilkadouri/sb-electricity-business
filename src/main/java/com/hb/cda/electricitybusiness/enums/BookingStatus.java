f08879528aabe9328cde21847cb73af24bf6c709
Nabil Kadouri <kadourinabil7@gmail.com>
2025-12-29 18:24:06 +0100

    fix(booking): adjust booking status enum

--- src/main/java/com/hb/cda/electricitybusiness/enums/BookingStatus.java
+++ src/main/java/com/hb/cda/electricitybusiness/enums/BookingStatus.java
@@ -15,19 +15,19 @@ public enum BookingStatus {
         this.displayValue = displayValue;
     }
 
-    @JsonValue // Cette annotation indique à Jackson d'utiliser cette méthode pour la sérialisation (Java -> JSON)
+    @JsonValue
     public String getDisplayValue() {
         return displayValue;
     }
 
-    @JsonCreator // Cette annotation indique à Jackson d'utiliser cette méthode pour la désérialisation (JSON -> Java)
+    @JsonCreator
     public static BookingStatus fromDisplayValue(String displayValue) {
         for (BookingStatus status : BookingStatus.values()) {
             if (status.displayValue.equalsIgnoreCase(displayValue)) {
                 return status;
             }
         }
-        // Gérer le cas où la valeur n'est pas trouvée, par exemple en lançant une exception
+
         throw new IllegalArgumentException("Statut de réservation inconnu : " + displayValue);
     }
 }
